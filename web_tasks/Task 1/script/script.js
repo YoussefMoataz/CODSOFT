e26beab9f74c83bc2e536beb5d52e805c658f3fa
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-08-14 14:36:54 +0300

    Adding portfolio: - Added navigation. - Added about section.

---
+++ web_tasks/Task 1/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $(".nav-list").on("click", "li", function(){
+
+        $(".nav-list").children().children(".active").removeAttr("class");
+        $(this).children().attr("class", "active");
+    });
+
+});
