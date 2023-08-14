4fa3b36604642f4af60ddb61108600250e12f23d
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-08-14 15:00:42 +0300

    Added links.

--- web_tasks/Task 1/script/script.js
+++ web_tasks/Task 1/script/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(".nav-list").on("click", "li", function(){
+    $(".nav-list").on("click", "li", function () {
 
         $(".nav-list").children().children(".active").removeAttr("class");
         $(this).children().attr("class", "active");
