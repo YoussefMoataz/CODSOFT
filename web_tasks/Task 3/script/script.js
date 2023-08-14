83cfd3570941c57d16e2b4f1ba336556f5d39dcc
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-08-14 10:29:40 +0300

    Added calculator UI. Added basic JS checks. Adding calculation function.

---
+++ web_tasks/Task 3/script/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $("button").on("click", function () {
+        var currentKey = $(this).data("value");
+
+        if (currentKey == "AC") {
+            $("#diaplay").val("0");
+            return;
+        } else if (currentKey == "DEL") {
+            $("#diaplay").val(function (index, value) {
+                return this.value.substr(0, value.length - 1);
+            });
+            return;
+        } else if (currentKey == "=") {
+            $("#diaplay").val(calculate($("#diaplay").val()));
+            return;
+        }
+
+        if($("#diaplay").val() == "0"){
+            $("#diaplay").val("");
+        }
+
+        $("#diaplay").val(function () {
+            return this.value + currentKey;
+        });
+    });
+
+});
+
+function calculate(exp){
+
+    let result = 0.0;
+
+    let numbers = [];
+    let operators = [];
+
+    // calculations here
+
+    return result;
+}
