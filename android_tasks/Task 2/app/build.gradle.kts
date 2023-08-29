e7668352bbc4943ab30a1f7d6d7894f59ab1a470
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-08-29 23:11:04 +0300

    .

--- android_tasks/Task 2/app/build.gradle.kts
+++ android_tasks/Task 2/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yquery.quote_of_the_day"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- android_tasks/Task 2/app/proguard-rules.pro
+++ android_tasks/Task 2/app/proguard-rules.pro
@@ -19,3 +19,12 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+
+# GSON Annotations
+-keepclassmembers class * {
+  @com.google.gson.annotations.SerializedName <fields>;
+}
+
+-dontwarn retrofit2.**
+-keep class retrofit2.** { *; }
